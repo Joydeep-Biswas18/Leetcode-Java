3c38c543c3a793524b88b43bd0814a4fc4395212
Joydeep Biswas <biswasjoydeep980@gmail.com>
2026-08-06 13:14:46 +0530

    Time: 17 ms (28.13%), Space: 59.7 MB (40.3%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int y = subarray(nums ,k);
+        int x = subarray(nums ,k-1);
+
+        return (y-x);
+    }
+    public static int subarray(int[]nums,int k){
+        nums = convert(nums);
+        int l=0; 
+        int r=0;
+        int count_subarray =0;
+        int count_ones =0;
+        //sliding Window and two pointer
+        if(k<0){
+            return 0;
+        }
+        while(r<nums.length){
+            count_ones += nums[r];
+            while(count_ones > k){
+                count_ones -= nums[l];
+                l++;
+            }
+            count_subarray = count_subarray+(r-l+1);
+            r++;
+        }
+        return count_subarray;
+        
+    }
+
+    
+
+    static int[] convert(int [] nums){
+        for( int i =0; i<nums.length; i++){
+            if(nums[i]%2==0){
+                nums[i]=0;
+            }
+            else{
+                nums[i]=1;
+            }
+        }
+
+        return nums;
+    }
+}
