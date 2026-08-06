d81b9bb4651b8c7ad097afccf2c257c320e03101
Joydeep Biswas <biswasjoydeep980@gmail.com>
2026-08-06 13:23:20 +0530

    Time: 8 ms (92.49%), Space: 59.8 MB (15.54%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int y = subarray(nums ,k);
-        int x = subarray(nums ,k-1);
+        // int y = subarray(nums ,k);
+        // int x = subarray(nums ,k-1);
 
-        return (y-x);
+        return subarray(nums,k) - subarray(nums ,k-1);
     }
     public static int subarray(int[]nums,int k){
-        nums = convert(nums);
+        // nums = convert(nums);
         int l=0; 
         int r=0;
         int count_subarray =0;
@@ -16,9 +16,9 @@ class Solution {
             return 0;
         }
         while(r<nums.length){
-            count_ones += nums[r];
+            count_ones += nums[r] & 1;
             while(count_ones > k){
-                count_ones -= nums[l];
+                count_ones -= nums[l] & 1;
                 l++;
             }
             count_subarray = count_subarray+(r-l+1);
@@ -30,16 +30,16 @@ class Solution {
 
     
 
-    static int[] convert(int [] nums){
-        for( int i =0; i<nums.length; i++){
-            if(nums[i]%2==0){
-                nums[i]=0;
-            }
-            else{
-                nums[i]=1;
-            }
-        }
+    // static int[] convert(int [] nums){
+    //     for( int i =0; i<nums.length; i++){
+    //         if(nums[i]%2==0){
+    //             nums[i]=0;
+    //         }
+    //         else{
+    //             nums[i]=1;
+    //         }
+    //     }
 
-        return nums;
-    }
+    //     return nums;
+    // }
 }
