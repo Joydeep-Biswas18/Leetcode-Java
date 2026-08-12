e72e440dd54698a9464100c40ee8c74812cc56ba
Joydeep Biswas <biswasjoydeep980@gmail.com>
2026-08-13 01:14:18 +0530

    Time: 75 ms (26.19%), Space: 88.5 MB (87.87%) - LeetHub

---
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSubarrayLength(int[] nums, int k) {
+        int l = 0;
+        int r = 0;
+        int max_length = 0;
+        if (k <= 0) {
+            return 0;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        while(r<nums.length){
+            map.put(nums[r], map.getOrDefault(nums[r],0)+1);
+            while(map.get(nums[r])>k){
+                
+                map.put(nums[l], map.get(nums[l])-1);
+
+                if(map.get(nums[l])==0){
+                    map.remove(nums[l]);
+                }
+                l++;
+            }
+            max_length = Math.max(max_length , r-l+1);
+            r++;
+        }
+        return max_length;
+
+    }
+}
