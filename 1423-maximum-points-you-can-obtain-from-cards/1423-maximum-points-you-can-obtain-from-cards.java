406e9e81313c309eb834a9fb0872511f0bd0ca7f
Joydeep Biswas <biswasjoydeep980@gmail.com>
2026-08-26 13:57:17 +0530

    Time: 2 ms (99.72%), Space: 61.4 MB (94.76%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.java
@@ -1,41 +1,26 @@
 class Solution {
     public int maxScore(int[] cardPoints, int k) {
+
         int n = cardPoints.length;
-        //Initialize tataol Sum as 0
-        int total_sum = 0;
-        for(int i =0; i<n;i++){
-            //Calculate total sum of all Elements in array 
-            total_sum +=cardPoints[i];
-        }
-        int Sum_min_subarray = Min_sum(cardPoints, k);
 
-        //Now substrack Sum of (n-k) size of subarray from total sum 
-        return total_sum - Sum_min_subarray;
+        int left_sum = 0;
 
-    }
-    //Find out the minimum Sum Subarray with N-k length
-    public static int Min_sum (int []nums , int k){
-        int n = nums.length;
-        int Min_sum = Integer.MAX_VALUE;
-        int sum =0;
-        int l =0;
-        int r =0;
-        while(r<nums.length){
-            sum+=nums[r];
-            if((r-l+1)> (n-k)){
-                sum -=nums[l];
-                l++;
-        
-            }
-            // Check only windows of exactly n-k elements
-            if ((r - l + 1) == (n-k)) {
-                Min_sum = Math.min(Min_sum, sum);
-            }
-            r++;
+        // Take all k cards from the left
+        for (int i = 0; i < k; i++) {
+            left_sum += cardPoints[i];
         }
 
-        
-        return Min_sum;
-    }
+        int max_sum = left_sum;
 
+        // Replace left cards one by one with right cards
+        for (int j = 0; j < k; j++) {
+
+            left_sum -= cardPoints[k - 1 - j];
+            left_sum += cardPoints[n - 1 - j];
+
+            max_sum = Math.max(max_sum, left_sum);
+        }
+
+        return max_sum;
+    }
 }
